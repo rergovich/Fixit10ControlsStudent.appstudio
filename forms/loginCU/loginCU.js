4ff84e58ecd3687601f565f5114940a874edc648
rergovich <rachelergovich@creighton.edu>
2020-03-24 16:08:10 -0500

    Program 1 Complete
    Completed all requirements for Program 1

--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -1,3 +1,21 @@
 
+req1 = Aja("http://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + txtNetID.value + "&j_password=" + pswdPassword.value)
 
 
+btnShortcutTofavFoods.onclick=function(){
+  ChangeForm(favFoods)
+}
+
+
+btnLogin.onclick=function(){
+  if (req1.status == 200) {
+    lblResponse.hidden = false
+    lblResponse.value = `Your LDAP return code was ${req1.responseText}`
+    ChangeForm(favFoods)
+  } else {
+      lblResponse.hidden = false
+      lblResponse.value = `Error: ${req1.status}`
+      inptNetID.value = ''
+      NSB.MsgBox('Please try again, your login was not valid.')
+  }
+}
